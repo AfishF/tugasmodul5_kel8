2aa330a958e42958e3a74e237298c874851354e1
Abbiyu Distira <abbiyukd@student.ce.undip.ac.id>
2020-12-01 16:00:54 +0700

    grid fix

--- src/Fetch.js
+++ src/Fetch.js
@@ -4,10 +4,6 @@ import axios from "axios";
 import { UserOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
-const gridStyle = {
-    width: '25%',
-  };
-
 export default class Fetch extends Component {
     constructor(props) {
         super(props);
@@ -24,10 +20,8 @@ export default class Fetch extends Component {
             portrayed: "",
         };
     }
-    
-    handleButton = (title) => {
-        alert(title);
-    };
+
+
     componentDidMount() {
         axios({
             method: "get",
@@ -49,41 +43,43 @@ export default class Fetch extends Component {
     render() {
         return (
             <div>
-                    <center>
-                        <h1>List of Breaking Bad Characters</h1>
-                    </center>
-                    {this.state.meth.map((results, index) => {
-                        return (
-                            <div className="site-card-wrapper" >    
-                                        <Card.Grid style={gridStyle}>
-                                            <center>
-                                            <Avatar src={results.img} size={256} icon={<UserOutlined />} />
-                                            <h2>{results.name}</h2>
-                                            </center>
-                                            <Row>
-                                                <Col span={12}><p>Birthday</p></Col>
-                                                <Col span={12}><p>: {results.birthday}</p></Col>
-                                            </Row>
-                                            <Row>
-                                                <Col span={12}><p>Status</p></Col>
-                                                <Col span={12}><p>: {results.status}</p></Col>
-                                            </Row>
-                                            <Row>
-                                                <Col span={12}><p>Nickname</p></Col>
-                                                <Col span={12}><p>: {results.nickname}</p></Col>
-                                            </Row>
-                                            <Row>
-                                                <Col span={12}><p>Occupation</p></Col>
-                                                <Col span={12}><p>: {results.occupation.join(', ')}</p></Col>
-                                            </Row>
-                                            <Row>
-                                                <Col span={12}><p>Portrayed</p></Col>
-                                                <Col span={12}><p>: {results.portrayed}</p></Col>
-                                            </Row>
-                                        </Card.Grid>
-                            </div>
-                        );
-                    })}
+                <center>
+                    <h1>List of Breaking Bad Characters</h1>
+                </center>
+                {this.state.meth.map((results, index) => {
+                    return (
+                        <div className="site-card-wrapper" key={results.char_id} >
+                        <Col span={12} offset={6}>    
+                            <Card>
+                                <center>
+                                    <Avatar src={results.img} size={256} icon={<UserOutlined />} />
+                                    <h2>{results.name}</h2>
+                                </center>
+                                <Row>
+                                    <Col span={12}><p>Birthday</p></Col>
+                                    <Col span={12}><p>: {results.birthday}</p></Col>
+                                </Row>
+                                <Row>
+                                    <Col span={12}><p>Status</p></Col>
+                                    <Col span={12}><p>: {results.status}</p></Col>
+                                </Row>
+                                <Row>
+                                    <Col span={12}><p>Nickname</p></Col>
+                                    <Col span={12}><p>: {results.nickname}</p></Col>
+                                </Row>
+                                <Row>
+                                    <Col span={12}><p>Occupation</p></Col>
+                                    <Col span={12}><p>: {results.occupation.join(', ')}</p></Col>
+                                </Row>
+                                <Row>
+                                    <Col span={12}><p>Portrayed</p></Col>
+                                    <Col span={12}><p>: {results.portrayed}</p></Col>
+                                </Row>
+                                </Card>
+                            </Col>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
